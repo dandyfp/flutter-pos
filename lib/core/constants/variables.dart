31783eb66180a5f13cddbf26d4fe8e1dead17158
dandyfirmansyah <dandyfp@sakutambah.id>
2024-04-25 20:37:19 +0700

    init project

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,6 @@
+class Variables {
+  static const String appName = 'Resto App';
+  static const String apiVersion = 'v1';
+  static const String baseUrl = 'http://192.168.132.156:8000';
+  // static const String baseUrl = 'https://resto.jagofullstack.com';
+}
